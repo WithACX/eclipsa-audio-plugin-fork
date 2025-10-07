e822ecf62d8a943822f72fbc57ad94937e3bbbf3
Joel Meuleman <joel.meuleman@a-cx.com>
2025-10-07 10:13:50 -0700

    Usable UI.

--- common/components/src/AudioFilePlayer.h
+++ common/components/src/AudioFilePlayer.h
@@ -2,6 +2,7 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
+#include "components/src/AudioPlayer.h"
 #include "components/src/ColouredSlider.h"
 #include "components/src/Icons.h"
 #include "components/src/RoundImageButton.h"
@@ -11,7 +12,8 @@ class AudioFilePlayer : public juce::Component {
   AudioFilePlayer()
       : playButton_("Play", IconStore::getInstance().getPlayIcon()),
         pauseButton_("Pause", IconStore::getInstance().getPauseIcon()),
-        stopButton_("Stop", IconStore::getInstance().getStopIcon()) {
+        stopButton_("Stop", IconStore::getInstance().getStopIcon()),
+        timeLabel_("timeLabel", "00:00 / 00:00") {
     playbackSlider_.setRange(0.0, 1.0);
     playbackSlider_.setValue(0.0);
     playbackSlider_.setSliderStyle(juce::Slider::LinearHorizontal);
@@ -27,6 +29,7 @@ class AudioFilePlayer : public juce::Component {
     addAndMakeVisible(playButton_);
     addAndMakeVisible(pauseButton_);
     addAndMakeVisible(stopButton_);
+    addAndMakeVisible(timeLabel_);
   }
 
   void paint(juce::Graphics& g) override {
@@ -41,43 +44,22 @@ class AudioFilePlayer : public juce::Component {
     playbackBounds_ = bounds.removeFromTop(bounds.getHeight() * 0.5f);
     volumeBounds_ = bounds;
 
-    int kButtonSz = JUCE_LIVE_CONSTANT(35);
-    int kButtonMargin = JUCE_LIVE_CONSTANT(10);
-    juce::FlexBox playbackFlex;
-    playbackFlex.flexDirection = juce::FlexBox::Direction::row;
-    playbackFlex.justifyContent = juce::FlexBox::JustifyContent::center;
-    playbackFlex.alignItems = juce::FlexBox::AlignItems::stretch;
-    playbackFlex.items.add(
-        juce::FlexItem(playButton_)
-            .withFlex(1.0f)
-            .withMaxWidth(kButtonSz)
-            .withMaxHeight(kButtonSz)
-            .withMargin(juce::FlexItem::Margin(0, kButtonMargin, 0, 0)));
-    playbackFlex.items.add(
-        juce::FlexItem(stopButton_)
-            .withFlex(1.0f)
-            .withMaxWidth(kButtonSz)
-            .withMaxHeight(kButtonSz)
-            .withMargin(juce::FlexItem::Margin(0, kButtonMargin, 0, 0)));
-    playbackFlex.items.add(
-        juce::FlexItem(playbackSlider_)
-            .withFlex(3.0f)
-            .withMinHeight(10.0f)
-            .withMinWidth(100.0f)
-            .withAlignSelf(juce::FlexItem::AlignSelf::center));
-    playbackFlex.performLayout(playbackBounds_);
+    int kButtonSz = (25);
+    int kButtonMargin = (10);
+    playButton_.setBounds(playbackBounds_.removeFromLeft(kButtonSz));
+    playbackBounds_.removeFromLeft(kButtonMargin);
+    stopButton_.setBounds(playbackBounds_.removeFromLeft(kButtonSz));
+    timeLabel_.setBounds(playbackBounds_.removeFromLeft(100));
+    playbackSlider_.setBounds(playbackBounds_);
 
-    juce::FlexBox volumeFlex;
-    volumeFlex.flexDirection = juce::FlexBox::Direction::row;
-    volumeFlex.justifyContent = juce::FlexBox::JustifyContent::flexStart;
-    volumeFlex.alignItems = juce::FlexBox::AlignItems::stretch;
-    volumeFlex.items.add(
-        juce::FlexItem(volumeSlider_).withFlex(1.0f).withMaxWidth(kButtonSz));
-    volumeFlex.performLayout(volumeBounds_);
+    speakerIcon_.setBounds(volumeBounds_.removeFromLeft(kButtonSz));
+    volumeSlider_.setBounds(
+        volumeBounds_.removeFromLeft(100).reduced(kButtonMargin));
   }
 
  private:
   RoundImageButton playButton_, pauseButton_, stopButton_;
+  juce::Label timeLabel_;
   ColouredSlider playbackSlider_, volumeSlider_;
   SpeakerImageComponent speakerIcon_;
   juce::Rectangle<int> playbackBounds_, volumeBounds_;
